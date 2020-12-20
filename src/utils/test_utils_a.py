a0088479d623a453fa93ae9123f0f4abfb30d2f7
ynouri <ynouri@users.noreply.github.com>
2020-12-20 01:56:31 -0500

    Enable Black and Flake8

--- src/utils/test_utils_a.py
+++ src/utils/test_utils_a.py
@@ -2,6 +2,11 @@ from utils.utils_a import sum_two_numbers
 from time import sleep
 
 
+MAX_NB_OF_CHARS_PER_LINE = [
+    "PEP-8 uses 79 characters --------- while Black uses 88"
+]
+
+
 def test_sum_two_numbers():
     assert sum_two_numbers(1, 5) == 6
     sleep(10)

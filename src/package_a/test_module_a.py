b69178a1815f432f1bed0696363f160654349fd0
ynouri <ynouri@users.noreply.github.com>
2020-12-20 00:12:56 -0500

    Add dummy modules

---
+++ src/package_a/test_module_a.py
@@ -0,0 +1,5 @@
+from package_a.module_a import Something
+
+
+def test_something():
+    something = Something()
---
+++ src/utils/test_utils_a.py
@@ -0,0 +1,4 @@
+from utils.utils_a import sum_two_numbers
+
+def test_sum_two_numbers():
+    assert sum_two_numbers(1, 5) == 6
